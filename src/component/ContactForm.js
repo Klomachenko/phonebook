8fb6562fc1044416d521248ae6db0c5d9abf35f0
Klomachenko <leekyumin0901@naver.com>
2024-08-27 03:33:00 +0900

    feat: add Contact to contact list:

--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useDispatch } from 'react-redux';
 
 const ContactForm = () => {
   const [name, setName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState(0);
+  const dispatch = useDispatch();
+
+  const addContact = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: 'ADD_CONTACT',
+      payload: { name, phoneNumber }, // 키, 값이 동일하면 이렇게 해도 됨 원래는{name:name, phoneNumber:phoneNumber}
+    });
+  };
 
   return (
     <div>
-      <Form>
+      <Form onSubmit={addContact}>
         <Form.Group className='mb-3' controlId='formName'>
           <Form.Label>이름</Form.Label>
           <Form.Control
